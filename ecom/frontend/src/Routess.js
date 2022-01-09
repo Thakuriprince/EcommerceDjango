0e13ed69df2a28af748b2f7b9370b594786c3ca2
Thakuriprince <princethakuri11111@gmail.com>
2022-01-09 09:48:00 +0545

    creating signup

--- ecom/frontend/src/Routess.js
+++ ecom/frontend/src/Routess.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './core/Home';
+import Signup from "./user/Signup";
 
 const Routess = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' exact element={<Home />} />
+                <Route path='/signup' exact element={<Signup />} />
             </Routes>
         </BrowserRouter>
     )
